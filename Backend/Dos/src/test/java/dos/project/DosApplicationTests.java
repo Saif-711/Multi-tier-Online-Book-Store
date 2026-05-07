194ce40afdefd88c89169b4cc8cab6aae584c7d7
Saif-711 <saifnairat23@gmail.com>
2026-05-07 15:47:37 +0100

    initialize backend

---
+++ Backend/Dos/src/test/java/dos/project/DosApplicationTests.java
@@ -0,0 +1,13 @@
+package dos.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
