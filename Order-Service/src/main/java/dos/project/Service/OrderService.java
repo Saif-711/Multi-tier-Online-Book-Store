205feb5f029d75376d5470dbc8c3f6ce52a2585e
Saif-711 <saifnairat23@gmail.com>
2026-05-07 17:10:45 +0100

    add catalog server

---
+++ Order-Service/src/main/java/dos/project/Service/OrderService.java
@@ -0,0 +1,9 @@
+package dos.project.Service;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderService {
+    public ResponseEntity<?> purchase()
+}
